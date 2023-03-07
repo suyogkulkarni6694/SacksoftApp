4597e168825c268448d468ee7a8d27c39da6ae85
suyog <suyogkulkarni6694@gmail.com>
2023-03-07 22:04:39 +0530

    Initial commit

---
+++ src/main/java/com/CrudDemo/SacksoftDemoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.CrudDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacksoftDemoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacksoftDemoCrudApplication.class, args);
+	}
+
+}
